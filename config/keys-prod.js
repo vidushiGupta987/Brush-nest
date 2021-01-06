3a71bb542a982857355958b07b89a3fb1720c302
VIDUSHI GUPTA <vidugpt987@gmail.com>
2021-01-06 21:00:40 +0530

    deployment to heroku

---
+++ config/keys-prod.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://onlinewebapp:onlinewebapp1@brush-nest.pnaiq.mongodb.net/brush-nest?retryWrites=true&w=majority',
+    GoogleClientID: '596114270579-i7rgiq5cuf3jeq81g2ib9h0qq9r6c2d2.apps.googleusercontent.com',
+    GoogleClientSecret: 'IDsMWUlXBiaRqBNbK5lQWTIK'
+}
