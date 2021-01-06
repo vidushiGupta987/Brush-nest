3ba69e7b472d2a162d6affae7f57f70d4839152f
VIDUSHI GUPTA <vidugpt987@gmail.com>
2021-01-07 00:08:01 +0530

    config keys added

--- config/keys-prod.js
+++ config/keys-prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MongoURI: 'mongodb+srv://onlinewebapp:onlinewebapp1@brush-nest.pnaiq.mongodb.net/brush-nest?retryWrites=true&w=majority',
-    GoogleClientID: '596114270579-i7rgiq5cuf3jeq81g2ib9h0qq9r6c2d2.apps.googleusercontent.com',
-    GoogleClientSecret: 'IDsMWUlXBiaRqBNbK5lQWTIK'
+    MongoURI: process.env.Mongo_URI,
+    GoogleClientID: process.env.GOOGLE_CLIENT_ID,
+    GoogleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
