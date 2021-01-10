9ea49f4040d5ac4362f4b2b5af578cc1bc10d3d4
VIDUSHI GUPTA <vidugpt987@gmail.com>
2021-01-10 23:41:32 +0530

    post page and user page modified

--- app.js
+++ app.js
@@ -207,10 +207,18 @@ app.put('/editingPost/:id', (req,res) => {
         });
     });
 });
-//handle post route
+//handle delete route
+app.delete('/:id', (req,res) => {
+    Post.remove({_id: req.params.id})
+    .then(() => {
+        res.redirect('profile');
+    });
+});
+//handle posts route
 app.get('/posts', ensureAuthentication, (req,res) => {
     Post.find({status:'public'})
     .populate('user')
+    .populate('comments.commentUser')
     .sort({date: 'desc'})
     .then((posts) => {
         res.render('publicPosts',{
@@ -218,6 +226,32 @@ app.get('/posts', ensureAuthentication, (req,res) => {
         });
     });
 });
+//display single user's post
+app.get('/showposts/:id', (req,res) => {
+    Post.find({user: req.params.id, status: 'public'})
+    .populate('user')
+    .sort({date: 'desc'})
+    .then((posts) => {
+        res.render('showUserPosts', {
+            posts: posts
+        });
+    } );
+});
+//save comments in database
+app.post('/addComment/:id', (req,res) => {
+    Post.findOne({_id: req.params.id})
+    .then((post) => {
+        const newComment = {
+            commentBody: req.body.commentBody,
+            commentUser: req.user._id
+        }      
+        post.comments.push(newComment)
+        post.save()
+        .then(() => {
+            res.redirect('/posts');
+        });
+    });
+}); 
   //handle user logout
 app.get('/logout',(req,res) => {
     req.logout();
